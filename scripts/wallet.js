f400a96e86e3de291353674cf1edfb2cd9f74f88
Winium <bonitejerwin5@gmail.com>
2023-08-28 11:27:01 +0800

    Show the user profile in html

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -1,3 +1,8 @@
 const username = JSON.parse(localStorage.getItem('username'));
 let balance = JSON.parse(localStorage.getItem('balance'));
 
+document.querySelector('.js-welcome-user')
+  .innerHTML = `Welcome, ${username}`;
+const htmlBalance = document.querySelector('.js-balance');
+
+htmlBalance.innerHTML = `Php ${balance}`;
