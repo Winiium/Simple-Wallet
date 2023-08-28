82165a78606438796b054261bc430eb06573aa87
Winium <bonitejerwin5@gmail.com>
2023-08-28 11:58:00 +0800

    Make income an expense button interactive

--- scripts/wallet.js
+++ scripts/wallet.js
@@ -1,8 +1,47 @@
+
 const username = JSON.parse(localStorage.getItem('username'));
 let balance = JSON.parse(localStorage.getItem('balance'));
 
 document.querySelector('.js-welcome-user')
   .innerHTML = `Welcome, ${username}`;
 const htmlBalance = document.querySelector('.js-balance');
+updateBalance();
 
-htmlBalance.innerHTML = `Php ${balance}`;
+const incomeButton = document.querySelector('.js-income');
+const expenseButton = document.querySelector('.js-expense');
+const amountDescription = document.querySelector('.js-amount-description');
+const selectedOption = document.querySelector('.js-selected-option');
+const inputAmount = document.querySelector('.js-input-amount');
+const doneButton = document.querySelector('.js-done-button');
+
+
+incomeButton.addEventListener('click', () => {
+  showInput(true);
+  selectedOption.innerHTML = 'Income';
+  doneButton.addEventListener('click', () => {
+    balance =+ Number(inputAmount.value);
+    showInput(false);
+  })
+});
+
+incomeButton.addEventListener('click', () => {
+  showInput(true);
+  selectedOption.innerHTML = 'Expense';
+  doneButton.addEventListener('click', () => {
+    balance =- Number(inputAmount.value);
+    showInput(false);
+  })
+});
+
+function updateBalance() {
+  htmlBalance.innerHTML = `Php ${balance}`;
+}
+
+function showInput(condition) {
+  if(condition) {
+    amountDescription.classList.add('amount-description-show');
+  } else {
+    amountDescription.classList.remove('amount-description-show');
+  }
+  
+}
